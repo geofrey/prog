c662cdf7ca91bc9db30bb87d09c1c5b1162a00a5
Geofrey Sanders <nuez.jr@gmail.com>
2013-05-11 16:25:00 -0600

    polish

--- badhandler.c
+++ badhandler.c
@@ -4,15 +4,28 @@
 
 int flag = 1;
 
-void *handle(int signal) {
+void handle(int signal) {
 	fprintf(stdout, "WHEEEE!\n");
 	flag = 0;
+	
+	/*
+	 * The only difference I see between exit(0)ing and returning is that the 
+	 * first loop down in main ticks one more time when we don't quit from the 
+	 * handler. This may be a race condition, as I'm sleeping one second each 
+	 * time in the child process and the parent is waiting a whole number of 
+	 * seconds before signalling.
+	 */
+	
+	/*
+	exit(0);
+	/*/
 	return; // this is incorrect - handler should not return
+	//*/
 }
 
 void setup() {
 	struct sigaction foo;
-	foo.sa_handler = &handle;
+	foo.sa_handler = handle;
 	sigemptyset(&(foo.sa_mask));
 	foo.sa_flags = 0;
 	
@@ -20,7 +33,7 @@ void setup() {
 	sigaction(SIGFPE, &foo, NULL);
 	return;
 }
-	
+
 int main(int argc, char **argv) {
 	setup();
 	
@@ -31,6 +44,9 @@ int main(int argc, char **argv) {
 		fprintf(stdout, "tock\n");
 		return 0;
 	} else {
+		sleep(5);
+		kill(kid, SIGILL); // SIGILLin' like a SIGVILLAIN
+		sleep(1);
 		wait(kid);
 	}
 	
