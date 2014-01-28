0300778433a30ec4429c1e1fb1ba7978869638b7
Geofrey Sanders <nuez.jr@gmail.com>
2014-01-28 10:21:36 -0500

    handle Excel's lifestyle choices about line endings

--- templateer.py
+++ templateer.py
@@ -3,16 +3,19 @@
 import sys
 
 
+def deexcelate(items):
+	return list(filter(lambda item: len(item)>0 and item != '\x00', items))
+
 def readclipboard_Cygwin():
-    clip = open('/dev/clipboard', 'r')
-    data = clip.readlines()
-    clip.close()
-    return data
+	clip = open('/dev/clipboard', 'r')
+	data = clip.readlines()
+	clip.close()
+	return deexcelate(data)
 
 def writeclipboard_Cygwin(data):
-    clip = open('/dev/clipboard', 'w')
-    clip.write(data)
-    clip.close()
+	clip = open('/dev/clipboard', 'w')
+	clip.write(data)
+	clip.close()
 
 def readclipboard_win32():
 	win32clipboard.OpenClipboard()
@@ -24,7 +27,7 @@ def readclipboard_win32():
 		raise Exception('no text on clipboard. format = {format}'.format(format=format))
 	data = win32clipboard.GetClipboardData(format)
 	win32clipboard.CloseClipboard()
-	return data.decode('utf-8').strip().split('\n') # utf-8 encoding is probably not appropriate for all formats, but this application only places ASCII/utf-8 anyway, so fugeddaboudit
+	return deexcelate(data.decode('utf-8').strip().split('\n')) # utf-8 encoding is probably not appropriate for all formats, but this application only places ASCII/utf-8 anyway, so fugeddaboudit
 
 def writeclipboard_win32(data):
 	win32clipboard.OpenClipboard()
@@ -40,7 +43,7 @@ elif sys.platform == 'win32':
 
 # sanity check
 def quicktest():
-	for teststring in ['foo', 'bar']:
+	for teststring in ['foo', 'bar', '', 'foo\nbar', ' \t    \t\t']:
 		writeclipboard(teststring)
 		echo = readclipboard()
 		if echo != teststring:
@@ -53,7 +56,7 @@ meta = {
 	'for': 'substitution into header and footer'
 	}
 header = 'stuff that goes first'
-format = 'what to do with the {elements} from each row of data'
+format = 'what to do with the {element}s from each row of data'
 footer = '{meta} that goes last'
 #PROTIP: leave these hints here and overwrite them later on down the file
 
@@ -63,19 +66,22 @@ def process(tabular, format, header='', footer='', meta={}):
 	names = {}
 	for index in range(len(labels)):
 		names[index] = labels[index]
-	#rows = [{names[index]: line[index] for index in range(len(line))} for line in lines]
+	#names = dict([(index, labels[index]) for index in range(len(labels))])
+	# wait...isn't that the same as using the list in the first place?
+	
+	#rows = [dict([(names[index], line[index]) for index in range(len(line))]) for line in lines]
 	rows = []
 	for line in lines:
 		row = {}
 		for index in range(len(line)):
 			row[names[index]] = line[index]
 		rows.append(row)
-	print(type(tabular))
-	print(tabular)
-	print(labels)
-	print(rows)
-	for row in rows:
-		print(dict(list(row.items())+list(meta.items())))
+	#print(type(tabular))
+	#print(tabular)
+	#print(labels)
+	#print(rows)
+	#for row in rows:
+	#	print(dict(list(row.items())+list(meta.items())))
 	return ''.join([
 		header.format(**meta),
 		''.join([format.format(**dict(list(row.items())+list(meta.items()))) for row in rows]),
@@ -83,14 +89,15 @@ def process(tabular, format, header='', footer='', meta={}):
 		])
 
 meta = {
-	'number': 'fifty-twelve',
-	'letter': 'letter to Santa',
-	't': '[TAB]'
+	't' : '\t',
 	}
-header = ''
-footer = ''
+header = '''var data = {{
+'''
 
-format = '{first}{t}{last}{t}{phone}{t}{company}{t}{t}{t}{t}'
+footer = '''}}'''
 
-#now that we've got all that settled
+format = '''{t}Util.mkPair("{valuationid}", {version}),
+'''
+	
+# now that we've got all that settled
 writeclipboard(process(readclipboard(), format, header, footer, meta))
