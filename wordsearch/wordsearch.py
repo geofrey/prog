b7ae4c4c56152f0b092036757c482ccee46e2703
Geofrey Sanders <nuez.jr@gmail.com>
2017-08-18 11:07:21 -0400

    show remaining letters

--- wordsearch/wordsearch.py
+++ wordsearch/wordsearch.py
@@ -1,13 +1,22 @@
+#!/usr/bin/python3
+
 import sys
 
 # read grid
 #  lines must all be the same length, naturally
 #  blank line to end
 grid = []
+shadow = []
 line = input()
 while len(line)>0:
     grid.append(list(line))
+    shadow.append([False for letter in line])
     line = input()
+
+class Location:
+    def __init__(self, coords, direction):
+        self.coords = coords
+        self.direction = direction
 
 # read words
 #  one word per line
@@ -72,14 +81,30 @@ for i in range(len(grid)):
                     break
                 cursor = cursor.next[letter]
                 if cursor.end:
-                    words[cursor.end] = ((i,j), direction)
+                    words[cursor.end] = Location((i,j), direction)
 
 # output $WORD $ROW,$COL $DIRECTION
 #  1,1 is top left corner
 #  'east' means right-to-left
 for word in wordlist:
     answer = words[word]
-    print(word,' not found!' if answer == None else (str(answer[0][0]+1)+','+str(answer[0][1]+1)+' '+answer[1]))
+    print(word,' not found!' if answer == None else (str(answer.coords[0]+1)+','+str(answer.coords[1]+1)+' '+answer.direction))
+
+for word in words:
+    if words[word] == None:
+        continue
+    i,j = words[word].coords
+    step = directions[words[word].direction]
+    for n in range(len(word)):
+        shadow[i][j] = True
+        i += step[0]
+        j += step[1]
+
+print()
+for i in range(len(grid)):
+    for j in range(len(grid[0])):
+        print(grid[i][j] if shadow[i][j] else ' ', end='')
+    print()
 
 # TODO
 #  case-insensitive search
