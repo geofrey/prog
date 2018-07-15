a0f2b6abee6759267adbeff37b2c5b47b407bbc5
gws <nuez.jr@gmail.com>
2018-07-15 17:17:18 -0400

    generalize commands

--- calculator-game/Calculator-game.py
+++ calculator-game/Calculator-game.py
@@ -1,6 +1,34 @@
 operations = {}
+operations['negate'] = lambda value: -value
+operations['delete'] = lambda value: int(value/10) # this might not always work
+operations['reverse'] = lambda value: int(str(value).reverse())
+
 def dispatch_operation(name):
-    operation = operations[name]
+    operation = None
+    if name in operations:
+        operation = operations[name]
+    elif name.startswith('add'):
+        term = int(name[3:])
+        operation = lambda value: value + term
+    elif name.startswith('div'):
+        denominator = float(name[3:])
+        operation = lambda value: value/denominator
+    elif name.startswith('edit'):
+        search, replace = name[4:].split('-')
+        operation = lambda value: float(str(value).replace(search, replace))
+    elif name.startswith('mul'):
+        factor = int(name[3:])
+        operation = lambda value: value*factor
+    elif name.startswith('sub'):
+        term = int(name[3:])
+        operation = lambda value: value - term
+    elif name.startswith('type'):
+        insert = name[4:]
+        operation = lambda value: float("{0:g}".format(value)+insert)
+    
+    if operation == None:
+        print('WARNING - operation {0} not defined'.format(name))
+    
     return lambda value: operation(value)
 
 class CalculatorPuzzle:
@@ -30,52 +58,6 @@ class CalculatorPuzzle:
     
     def __str__(self):
         return '{0} -> {1} in {2} using [{3}]'.format(self.initial, self.goal, self.moves, ', '.join(self.buttons))
-    
-operations['add1'] = lambda value: value+1
-operations['add2'] = lambda value: value+2
-operations['add3'] = lambda value: value+3
-operations['add4'] = lambda value: value+4
-operations['add5'] = lambda value: value+5
-operations['add6'] = lambda value: value+6
-operations['add8'] = lambda value: value+8
-operations['add9'] = lambda value: value+9
-
-operations['delete'] = lambda value: int(value/10) # this might not always work
-
-operations['div2'] = lambda value: value/2.0
-operations['div3'] = lambda value: value/3.0
-operations['div4'] = lambda value: value/4.0
-operations['div5'] = lambda value: value/5.0
-operations['div7'] = lambda value: value/7.0
-
-operations['edit1-2'] = lambda value: float(str(value).replace('1', '2'))
-operations['edit2-3'] = lambda value: float(str(value).replace('2', '3'))
-operations['edit6-9'] = lambda value: float(str(value).replace('6', '9'))
-operations['edit8-4'] = lambda value: float(str(value).replace('8', '4'))
-
-operations['mul2'] = lambda value: value*2
-operations['mul3'] = lambda value: value*3
-operations['mul4'] = lambda value: value*4
-operations['mul5'] = lambda value: value*5
-operations['mul7'] = lambda value: value*7
-operations['mul9'] = lambda value: value*9
-operations['mul10'] = lambda value: value*10
-operations['mul11'] = lambda value: value*11
-
-operations['negate'] = lambda value: -value
-
-operations['sub2'] = lambda value: value-2
-operations['sub5'] = lambda value: value-5
-operations['sub8'] = lambda value: value-8
-operations['sub9'] = lambda value: value-9
-
-operations['type0'] = lambda value: value*10+0
-operations['type1'] = lambda value: value*10+1
-operations['type2'] = lambda value: value*10+2
-operations['type5'] = lambda value: value*10+5
-operations['type10'] = lambda value: value*100+10
-operations['type12'] = lambda value: value*100+12
-
 
 puzzles = {}
 
@@ -93,9 +75,6 @@ for line in datafile.readlines():
     goal = int(line[2].strip())
     initial = int(line[3].strip())
     buttons = map(lambda token: token.strip(), line[4:])
-    for button in buttons:
-        if button not in operations:
-            print('WARNING - puzzle {0} operation {1} is not defined'.format(id, button))
     
     puzzles[id] = CalculatorPuzzle(moves, goal, initial, buttons)
 
